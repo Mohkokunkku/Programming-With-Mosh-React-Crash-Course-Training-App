051c01717289c02ab6d5c9163ca59c1b504ff5a6
Riku Räihä <riku.raiha@copla.fi>
2019-07-25 14:22:03 +0300

    Add project files.

---
+++ React1/ClientApp/src/components/counters.jsx
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+import Counter from './counter';
+
+
+class Counters extends Component {
+
+
+
+
+    render() {
+        return (<div>
+            <button onClick={this.props.OnReset} className="btn btn-primary btn-sm m-2">Reset!</button>
+            {this.props.counters.map(counter => (<Counter onIncrement={this.props.onIncrement} onDelete={this.props.onDelete} key={counter.id} counter={counter}>
+                <h4>Counter #{counter.id}</h4>
+            </Counter>))}
+        </div>)
+    }
+}
+
+export default Counters;
---
+++ React1/ClientApp/src/components/navbar.jsx
@@ -0,0 +1,23 @@
+﻿import React, { Component } from 'react';
+
+const NavBar = (props) => {
+    return (<nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar
+            <span className="badge badge-pill badge-primary">Luku: {props.totalCounter}</span>
+        </a>
+    </nav>)
+}
+
+//class NavBar extends Component {
+//    state = {}
+//    render() {
+//        return (<nav className="navbar navbar-light bg-light">
+//            <a className="navbar-brand" href="#">Navbar
+//            <span className="badge badge-pill badge-primary">Luku: {this.props.totalCounter}</span>
+//            </a>
+//        </nav>)
+//    }
+//}
+
+export default NavBar;
+
